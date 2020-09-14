54430d25140c2821bb1f163853ddf72258024da5
Bianca Wilkinson <Bianca.Wilkinson@gmail.com>
2020-09-14 14:34:42 +1000

    Added a dashboard with Netlify deployment capability

---
+++ dashboardConfig.js
@@ -0,0 +1,30 @@
+export default {
+  widgets: [
+    {
+      name: "netlify",
+      options: {
+        title: "Website Deployments",
+        sites: [
+          {
+            title: "mmlinedancing.com",
+            apiId: "f3f9e63f-3721-43f0-b54a-b3dd6de6628d",
+            buildHookId: "5f5eee50a9ea4dd836e47dfe",
+            name: "mmlinedancing-v2",
+          },
+          {
+            title: "admin.mmlinedancing.com",
+            apiId: "a307a69a-281d-47e4-b227-f8cd7d1f870b",
+            buildHookId: "5f5ef07a3b4b0ec2ca28def3",
+            name: "mmadmin",
+          },
+        ],
+      },
+    },
+    {
+      name: "project-info",
+    },
+    {
+      name: "project-users",
+    },
+  ],
+}
